2f273754a184d8c6b513fd0132e4930f88ad1f02
lpeterson52 <111457379+lpeterson52@users.noreply.github.com>
2024-05-20 16:33:40 -0700

    added bar chart

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 let x = Math.random() * 100;
 let winstreak = 0;
 let highscore = 0;
-function randint(){
+
+
+function randint() {
     x = Math.random() * 100
     document.getElementById("demo").innerHTML = x;
-    if(x > winstreak){
+    if (x > winstreak) {
         winstreak++;
     }
-    else{
-        if(winstreak > highscore){
+    else {
+        if (winstreak > highscore) {
             highscore = winstreak;
         }
         winstreak = 0;
-        
+
     }
     document.getElementById("wins").innerHTML = winstreak;
     document.getElementById("highscore").innerHTML = highscore;
@@ -21,14 +23,14 @@ function randint(){
 
 
 
-function calculateOdds(streak){
-    if(streak == 0){
-        return 1 - (streak/100);
+function calculateOdds(streak) {
+    if (streak == 0) {
+        return 1 - (streak / 100);
     }
-    return (1-(streak/100)) * calculateOdds(streak-1);
+    return (1 - (streak / 100)) * calculateOdds(streak - 1);
 
 }
 
-function setCalculatedOdds(streak){
+function setCalculatedOdds(streak) {
     document.getElementById("odds").innerHTML = calculateOdds(streak) * 100 + "%";
 }
